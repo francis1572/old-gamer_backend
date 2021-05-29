76bf63cea79a19d4c295d35db5efd33eda30bd1a
姜皜 <b05705010@ntu.edu.tw>
2021-05-29 22:13:11 +0800

    model

--- models/system.go
+++ models/system.go
@@ -1,5 +1,7 @@
 package models
 
+import "go.mongodb.org/mongo-driver/bson"
+
 type System struct {
 	TotalVotes    int   `bson:"totalVotes" json:"totalVotes"`
 	VoteThreshold int64 `bson:"voteThreshold" json:"voteThreshold"`
@@ -14,4 +16,15 @@ type Notification struct {
 	PostId     string `bson:"postId" json:"postId"`
 	Floor      int64  `bson:"floor" json:"floor"`
 	VoteId     string `bson:"voteId" json:"voteId"`
+}
+
+func (a *Notification) TableName() string {
+	return "Notification"
+}
+
+func (a *Notification) ToQueryBson() bson.M {
+	queryObject := bson.M{
+		"userId": a.UserId,
+	}
+	return queryObject
 }
